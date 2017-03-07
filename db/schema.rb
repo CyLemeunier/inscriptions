bf430069e16d80ddb74c5fff45414297f115a78a
Cyrielle Lemeunier <dev@lemeunier.org>
2017-03-07 20:04:41 +0100

    add column address to tournaments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925183324) do
+ActiveRecord::Schema.define(version: 20170307190359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160925183324) do
     t.date     "date2"
     t.date     "deadline"
     t.text     "comment"
+    t.string   "address"
   end
 
   create_table "transactions", force: :cascade do |t|
